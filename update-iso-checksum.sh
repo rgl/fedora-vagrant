dbaeaf2eb19d8fe1aa467b98eee8724ffe36c192
Rui Lopes <rgl@ruilopes.com>
2022-03-20 09:27:31 +0000

    upgrade to fedora 35

--- update-iso-checksum.sh
+++ update-iso-checksum.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # this will update the fedora.json file with the current netboot image checksum.
-# see https://getfedora.org/keys/
-set -eux
-version=31-1.9
-gpg --keyserver hkp://keys.fedoraproject.org --recv-keys 0x3C3359C4
-iso_url=$(jq -r '.variables.iso_url' fedora.json)
+# see https://getfedora.org/security/
+set -euxo pipefail
+wget -qO- https://getfedora.org/static/fedora.gpg | gpg --import
+version="$(jq -r '.variables.iso_url' fedora.json | perl -ne '/-(\d+.+)\.iso$/ && print $1')"
+iso_url="$(jq -r '.variables.iso_url' fedora.json)"
 iso_checksum_url="$(dirname $iso_url)/Fedora-Server-$version-x86_64-CHECKSUM"
 iso_checksum_file=$(basename $iso_checksum_url)
 curl -O -L --silent --show-error $iso_checksum_url
 gpg --verify $iso_checksum_file
 iso_checksum=$(grep -E "SHA256.+$(basename $iso_url)" $iso_checksum_file | awk '{print $4}')
-sed -i -E "s,(\"iso_checksum\": \")([a-f0-9]*)(\"),\\1$iso_checksum\\3,g" fedora.json
+sed -i -E "s,(\"iso_checksum\": \").*(\"),\\1sha256:$iso_checksum\\2,g" fedora.json
 rm $iso_checksum_file
 echo 'iso_checksum updated successfully'
